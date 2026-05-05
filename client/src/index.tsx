85a1dba8c485c277d020fda6f9b84afadb1ea75e
vishnukallam <emailme.vishnuvardhan@gmail.com>
2026-05-05 16:40:18 +0530

    feat: initialize client application with custom ResizeObserver and define core type interfaces

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+
+// Suppress ResizeObserver errors commonly caused by xterm.js / monaco-editor
+const _ResizeObserver = window.ResizeObserver;
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+  constructor(callback: ResizeObserverCallback) {
+    super((entries, observer) => {
+      window.requestAnimationFrame(() => {
+        callback(entries, observer);
+      });
+    });
+  }
+};
+
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
